e807baed4f5243dfec95445da3e35af3169e8e99
Stewen <784547228@qq.com>
2023-10-24 10:34:29 +0800

    ios share

--- ios/Classes/SharingFile.swift
+++ ios/Classes/SharingFile.swift
@@ -8,23 +8,23 @@
 class SharingFile: Codable {
     var value: String
     var thumbnail: String?; // video thumbnail
-    var duration: Double?; // video duration in milliseconds
+    var duration: Int?; // video duration in milliseconds
     var type: SharingFileType;
 
     enum CodingKeys: String, CodingKey {
-        case value = "path"
+        case value
         case thumbnail
         case duration
         case type
     }
-    
-    init(value: String, thumbnail: String?, duration: Double?, type: SharingFileType) {
+
+    init(value: String, thumbnail: String?, duration: Int?, type: SharingFileType) {
         self.value = value
         self.thumbnail = thumbnail
         self.duration = duration
         self.type = type
     }
-    
+
     // Debug method to print out SharedMediaFile details in the console
     func toString() {
         print("[SharingFile] \n\tvalue: \(self.value)\n\tthumbnail: \(self.thumbnail ?? "--" )\n\tduration: \(self.duration ?? 0)\n\ttype: \(self.type)")
